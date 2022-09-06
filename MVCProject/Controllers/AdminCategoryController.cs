4558d57a4f3d612501fbf289fe8c5c446fd8bcb4
MuratDuroglu <muratduroglu@icloud.com>
2022-09-07 00:26:09 +0300

    validationrules

--- MVCProject/Controllers/AdminCategoryController.cs
+++ MVCProject/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concreate;
+using EntityLayer.Concreate;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,36 @@ namespace MVCProject.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager categorymanager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categorylist = categorymanager.GetAll();
+
+            return View(categorylist);
+        }
+
+       
+        public ActionResult AddCategory(Category category)
+        {
+            
+
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult result = categoryvalidator.Validate(category);
+            if (result.IsValid)
+            {
+                categorymanager.Add(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                
+            }
             return View();
         }
+
     }
 }
